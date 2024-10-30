3a3dec07ef7fa00fd32ebde03048d91550847e2f
vanesamrd <paneja435@gmail.com>
2024-10-30 08:35:50 +0700

    Bilangan

---
+++ ArrayBilangan23.java
@@ -0,0 +1,19 @@
+/**
+ * ArrayBilangan23
+ */
+public class ArrayBilangan23 {
+
+    public static void main(String[] args) {
+        int[] bil = new int[4];
+        
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -17;
+        bil[3] = 17;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
