548bf6369359f12bc5d6cd06dd94ab079f64ff01
vanesamrd <paneja435@gmail.com>
2024-10-30 08:49:37 +0700

    modifikasi

--- ArrayBilangan23.java
+++ ArrayBilangan23.java
@@ -4,16 +4,15 @@
 public class ArrayBilangan23 {
 
     public static void main(String[] args) {
-        int[] bil = new int[4];
+        double[] bil = new double[4];
         
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -17;
-        bil[3] = 17;
+        bil[0] = 5.0;
+        bil[1] = 12867;
+        bil[2] = 7.5;
+        bil[3] = 2000000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+       for (int i = 0; i <= 4; i++){
+        System.out.println(bil[i]);
+       }
     }
 }
